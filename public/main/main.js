af80f637d8ae6c90ea201071bfebb1ea37468e8b
Your Name <zeroanomale@gmail.com>
2025-07-07 13:52:47 +0500

    Update

--- public/main/main.js
+++ public/main/main.js
@@ -1,8 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {  
   const profileBtn = document.getElementById("profile_button");
   const backBtn = document.getElementById("back_button");
   const profileView = document.getElementById("profile_view");
   const userEmail = document.getElementById("user_email");
+
+  const settingbtn = document.getElementById('setting_button');
+  const backbtn = document.getElementById('back_button_set');
+  const profile_overlay_set = document.getElementById('profile_overlay_set');
+  const settingDiv = document.getElementById('setting_div');
+  const change_pass = document.getElementById('change_the_password');
+  const change_language = document.getElementById('change_the_language');
+  const logout = document.getElementById('logout');
+  const delete_account = document.getElementById('delete_account');
+
+  profile_overlay_set.style.display = "none";
+
+  settingbtn.addEventListener("click", () => {
+    const token = localStorage.getItem("token")
+    profile_overlay_set.style.display = "flex";
+  })
+
+  backbtn.addEventListener("click", () => {
+    profile_overlay_set.style.display = "none";
+  });
+
 
   profileBtn.addEventListener("click", () => {
     const token = localStorage.getItem("token");
