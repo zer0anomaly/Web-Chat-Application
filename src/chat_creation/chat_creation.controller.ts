8f8e32ad3f7432083a84238b149d0673f84d9e01
zer0anomaly <zeroanomale@gmail.com>
2025-07-19 12:47:05 +0500

    Update

--- src/chat_creation/chat_creation.controller.ts
+++ src/chat_creation/chat_creation.controller.ts
@@ -3,32 +3,5 @@ import { ChatCreationService } from './chat_creation.service';
 import { CreateChatCreationDto } from './dto/create-chat_creation.dto';
 import { UpdateChatCreationDto } from './dto/update-chat_creation.dto';
 
-@Controller('chat-creation')
-export class ChatCreationController {
-  constructor(private readonly chatCreationService: ChatCreationService) {}
-
-  @Post()
-  create(@Body() createChatCreationDto: CreateChatCreationDto) {
-    return this.chatCreationService.create(createChatCreationDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.chatCreationService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.chatCreationService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateChatCreationDto: UpdateChatCreationDto) {
-    return this.chatCreationService.update(+id, updateChatCreationDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.chatCreationService.remove(+id);
-  }
-}
+@Controller('chat_creation')
+  export class ChatCreation()
