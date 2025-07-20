c0c38d090f8c1864b535aa178df7c6292149e23d
zer0anomaly <zeroanomale@gmail.com>
2025-07-20 05:13:33 +0500

    Update

--- src/chat_creation/dto/create-chat_creation.dto.ts
+++ src/chat_creation/dto/create-chat_creation.dto.ts
@@ -1,1 +1,14 @@
-export class CreateChatCreationDto {}
+import { IsString, IsNotEmpty, IsEmail } from 'class-validator'
+
+export class CreateChatCreationDto {
+	@IsString()
+	@IsNotEmpty()
+	@IsEmail()
+	user1: string;
+
+
+	@IsString()
+	@IsNotEmpty()
+	@IsEmail()
+	user2: string;
+}
