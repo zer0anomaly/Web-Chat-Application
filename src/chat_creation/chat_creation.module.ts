daf58a84df8476f28b0f7de19439ac80ea7f00e9
zer0anomaly <zeroanomale@gmail.com>
2025-08-16 09:49:28 +0500

    Update

--- src/chat_creation/chat_creation.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ChatCreationService } from './chat_creation.service';
-import { ChatCreationController } from './chat_creation.controller';
-
-@Module({
-  controllers: [ChatCreationController],
-  providers: [ChatCreationService],
-})
-export class ChatCreationModule {}
--- src/chat_creation/dto/update-chat_creation.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateChatCreationDto } from './create-chat_creation.dto';
-
-export class UpdateChatCreationDto extends PartialType(CreateChatCreationDto) {}
